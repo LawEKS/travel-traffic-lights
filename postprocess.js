a67a0b7dd7839c24a6717180d6145aacb15fa0a5
Lawrence Sarpong <lawrence.sarpong@fluidly.com>
2021-06-02 23:29:55 +0100

    Add logs

--- postprocess.js
+++ postprocess.js
@@ -8,12 +8,14 @@ try {
   const doc = new DOMParser().parseFromString(html, "text/html")
 
   const tables = doc.querySelectorAll("table")
-
+  console.log("number of tables", tables.length)
   const data = [...tables].map((table) => {
     const status = table
       .querySelector("thead th:first-child")
       .textContent.split(" ")[0]
       .toLowerCase()
+
+    console.log("status", status)
 
     const countries = [...table.querySelectorAll("tbody th:first-child")].map(
       (cell) => cell.textContent.trim()
