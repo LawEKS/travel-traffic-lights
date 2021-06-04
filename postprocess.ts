7686e1a42d4f86095ab12eb2645aec47571d5065
Lawrence <lawrenceeks@yahoo.com>
2021-06-05 00:32:28 +0100

    Remove error file if present

--- postprocess.ts
+++ postprocess.ts
@@ -1,13 +1,18 @@
-import { parseDoc } from "./parse-doc.ts"
-import { writeJSON, writeTXT, removeFile } from "https://deno.land/x/flat/mod.ts"
+import { parseDoc } from "./parse-doc.ts";
+import {
+  writeJSON,
+  writeTXT,
+  removeFile,
+} from "https://deno.land/x/flat/mod.ts";
 
 try {
-  const filename = Deno.args[0]
-  const html = await Deno.readTextFile(filename)
-  const data = parseDoc(html)
-  await writeJSON("travel-traffic-lights.json", data)
-  await removeFile(filename)
+  const filename = Deno.args[0];
+  const html = await Deno.readTextFile(filename);
+  const data = parseDoc(html);
+  await writeJSON("travel-traffic-lights.json", data);
+  await removeFile(filename);
+  await removeFile("errors.txt").catch(() => {});
 } catch (error) {
-  console.log(error)
-  await writeTXT("errors.txt", error)
+  console.log(error);
+  await writeTXT("errors.txt", error);
 }
